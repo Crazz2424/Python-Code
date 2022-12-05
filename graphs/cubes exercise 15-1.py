a431a1824cf6ff37fa486e29790cd259f6e2cf26
Crazz2424 <86924457+Crazz2424@users.noreply.github.com>
2022-12-05 09:06:16 -0500

    Learning Logs Chap 19

--- graphs/cubes exercise 15-1.py
+++ graphs/cubes exercise 15-1.py
@@ -8,9 +8,9 @@ fig, ax = plt.subplots()
 ax.plot(input_values, cubes, linewidth=3)
 
 # Set chart title and label axes
-ax.set_title("Square Numbers", fontsize=24)
+ax.set_title("Cube Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel ("Square of Value", fontsize=14)
+ax.set_ylabel ("Cube of Value", fontsize=14)
 
 # Set size of tick labels
 ax.tick_params(axis='both', labelsize=14)
