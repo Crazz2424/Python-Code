df87fdb3bcaa14ceb83e7e68e6275dfdb663000b
Crazz2424 <86924457+Crazz2424@users.noreply.github.com>
2022-12-19 08:27:29 -0500

    EQ_Web update 3

--- EQ_Website/EQ_Website/settings.py
+++ EQ_Website/EQ_Website/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+    'EQ_Website',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
