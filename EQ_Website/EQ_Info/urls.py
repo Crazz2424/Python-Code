6a5eeb45335eef3215db481dd7f2c0c26298e685
Crazz2424 <86924457+Crazz2424@users.noreply.github.com>
2022-12-19 10:00:12 -0500

    EQ Website delete

--- EQ_Website/EQ_Info/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from EQ_Info.views import EQ_page
-
-# URLConf
-urlpatterns = [
-    path('', EQ_page),
-]
--- EQ_Website/EQ_Info/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def EQ_page(request):
-    return render(request,'EQ_page.html'),
-    
