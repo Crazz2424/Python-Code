8d82d86192e5c5effba04010ba85a6b0f990875c
Crazz2424 <86924457+Crazz2424@users.noreply.github.com>
2022-12-22 08:09:46 -0500

    EQ Website Update possible error

--- EQWebProject/EQ_Website/EQ_Website/settings.py
+++ EQWebProject/EQ_Website/EQ_Website/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'EQ_Website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [BASE_DIR, 'templates', 'static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
